e69bb4872663df254cd27edc7f08263fbc38109d
wb.lidongheng <wb.lidongheng@mesg.corp.netease.com>
2017-11-29 20:29:51 +0800

    第一次建好了models

--- develop/FootballmatchAnalysing/analysing/models.py
+++ develop/FootballmatchAnalysing/analysing/models.py
@@ -1,3 +1,120 @@
+#coding:utf-8
 from django.db import models
 
 # Create your models here.
+class League(models.Model):
+    league = models.CharField(max_length=20)
+
+class Team(models.Model):
+    name = models.CharField(max_length=40)
+    league = models.ForeignKey(League)
+    comment = models.TextField()
+
+class Player(models.Model):
+    name = models.CharField(max_length=200)
+    team = models.CharField(max_length=40)
+    homeland = models.CharField(max_length=40)
+    main_position = models.CharField(max_length=2)
+    main_position_short = models.CharField(max_length=2)
+    current_position = models.CharField(max_length=3,null=True)
+    other_position = models.CharField(max_length=3,null=True)
+    another_pstion = models.CharField(max_length=3,null=True)
+    tall = models.SmallIntegerField(default=0)
+    weight = models.SmallIntegerField(default=0)
+    game = models.SmallIntegerField(default=0)
+    first = models.SmallIntegerField(default=0)
+    substitute = models.SmallIntegerField(default=0)
+    goal = models.SmallIntegerField(default=0)
+    assists = models.SmallIntegerField(default=0)
+    yellowcard = models.SmallIntegerField(default=0)
+    redcard = models.SmallIntegerField(default=0)
+    other = models.CharField(max_length=200)
+    comment = models.TextField()
+
+class PremierLeague(models.Model):
+	  team = models.ForeignKey(Team)
+    round = models.SmallIntegerField(default=0)
+    win = models.SmallIntegerField(default=0)
+    tie = models.SmallIntegerField(default=0)
+    defeat = models.SmallIntegerField(default=0)
+    goal = models.SmallIntegerField(default=0)
+    lose = models.SmallIntegerField(default=0)
+    diff = models.SmallIntegerField(default=0)
+    point = models.SmallIntegerField(default=0)
+
+class LaLiga(models.Model):
+    team = models.ForeignKey(Team)
+    round = models.SmallIntegerField(default=0)
+    win = models.SmallIntegerField(default=0)
+    tie = models.SmallIntegerField(default=0)
+    defeat = models.SmallIntegerField(default=0)
+    goal = models.SmallIntegerField(default=0)
+    lose = models.SmallIntegerField(default=0)
+    diff = models.SmallIntegerField(default=0)
+    point = models.SmallIntegerField(default=0)
+
+class SerieA(models.Model):
+    team = models.ForeignKey(Team)
+    round = models.SmallIntegerField(default=0)
+    win = models.SmallIntegerField(default=0)
+    tie = models.SmallIntegerField(default=0)
+    defeat = models.SmallIntegerField(default=0)
+    goal = models.SmallIntegerField(default=0)
+    lose = models.SmallIntegerField(default=0)
+    diff = models.SmallIntegerField(default=0)
+    point = models.SmallIntegerField(default=0)
+
+class Bundesliga(models.Model):
+    team = models.ForeignKey(Team)
+    round = models.SmallIntegerField(default=0)
+    win = models.SmallIntegerField(default=0)
+    tie = models.SmallIntegerField(default=0)
+    defeat = models.SmallIntegerField(default=0)
+    goal = models.SmallIntegerField(default=0)
+    lose = models.SmallIntegerField(default=0)
+    diff = models.SmallIntegerField(default=0)
+    point = models.SmallIntegerField(default=0)
+
+class Bundesliga(models.Model):
+    team = models.ForeignKey(Team)
+    round = models.SmallIntegerField(default=0)
+    win = models.SmallIntegerField(default=0)
+    tie = models.SmallIntegerField(default=0)
+    defeat = models.SmallIntegerField(default=0)
+    goal = models.SmallIntegerField(default=0)
+    lose = models.SmallIntegerField(default=0)
+    diff = models.SmallIntegerField(default=0)
+    point = models.SmallIntegerField(default=0)
+
+class Franchleaue(models.Model):
+    team = models.ForeignKey(Team)
+    round = models.SmallIntegerField(default=0)
+    win = models.SmallIntegerField(default=0)
+    tie = models.SmallIntegerField(default=0)
+    defeat = models.SmallIntegerField(default=0)
+    goal = models.SmallIntegerField(default=0)
+    lose = models.SmallIntegerField(default=0)
+    diff = models.SmallIntegerField(default=0)
+    point = models.SmallIntegerField(default=0)
+
+class JLeague(models.Model):
+    team = models.ForeignKey(Team)
+    round = models.SmallIntegerField(default=0)
+    win = models.SmallIntegerField(default=0)
+    tie = models.SmallIntegerField(default=0)
+    defeat = models.SmallIntegerField(default=0)
+    goal = models.SmallIntegerField(default=0)
+    lose = models.SmallIntegerField(default=0)
+    diff = models.SmallIntegerField(default=0)
+    point = models.SmallIntegerField(default=0)
+
+class ChinaFootballLeague(models.Model):
+    team = models.ForeignKey(Team)
+    round = models.SmallIntegerField(default=0)
+    win = models.SmallIntegerField(default=0)
+    tie = models.SmallIntegerField(default=0)
+    defeat = models.SmallIntegerField(default=0)
+    goal = models.SmallIntegerField(default=0)
+    lose = models.SmallIntegerField(default=0)
+    diff = models.SmallIntegerField(default=0)
+    point = models.SmallIntegerField(default=0)
--- develop/FootballmatchAnalysing/match/models.py
+++ develop/FootballmatchAnalysing/match/models.py
@@ -5,6 +5,31 @@ from django.db import models
 class Match(models.Model):
     weekday = models.CharField(max_length=20)
     number = models.IntegerField(default=101)
-    time = models.CharField(max_length=5)
+    time = models.CharField(max_length=30)
     host_team = models.CharField(max_length=40)
     away_team = models.CharField(max_length=40)
+
+class Historymatch(models.Model):
+    date = models.CharField(max_length=30)
+    league = models.CharField(max_length=20)
+    host_team = models.CharField(max_length=40)
+    host_goal = models.SmallIntegerField(default=0)
+    away_team = models.CharField(max_length=40)
+    away_goal = models.SmallIntegerField(default=0)
+    handicap = models.CharField(max_length=16,null=True)
+    handicap_result = models.CharField(max_length=4,null=True)
+
+class Futurematch(models.Model):
+    date = models.CharField(max_length=30)
+    league = models.CharField(max_length=20)
+    host_team = models.CharField(max_length=40)
+    host_goal = models.SmallIntegerField(default=0)
+    away_team = models.CharField(max_length=40)
+    away_goal = models.SmallIntegerField(default=0)
+    handicap = models.CharField(max_length=16,null=True)
+    handicap_result = models.CharField(max_length=4,null=True)
+
+class Odd(models.Model)
+    match = models.ForeignKey(Historymatch)
+    text = models.TextField()
+
