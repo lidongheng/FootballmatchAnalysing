d9879220bcd1bd22f6b1cfe87de231e6b7e8f30b
wb.lidongheng <wb.lidongheng@mesg.corp.netease.com>
2017-11-30 17:43:03 +0800

    修改数据库的配置

--- develop/FootballmatchAnalysing/FootballmatchAnalysing/settings.py
+++ develop/FootballmatchAnalysing/FootballmatchAnalysing/settings.py
@@ -81,14 +81,14 @@ WSGI_APPLICATION = 'FootballmatchAnalysing.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': "django.db.backends.mysql",
-        'NAME': "test1117",
-        'USER': 'root',
-        'PASSWORD': '88888888',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': "django.db.backends.mysql",
+        #'NAME': "test1117",
+        #'USER': 'root',
+        #'PASSWORD': '88888888',
+        #'HOST': 'localhost',
+        #'PORT': '3306',
     }
 }
 
