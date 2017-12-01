eb9c565de8d436acb4d05143e8a1a56490bbd2f2
Dongheng Li <huoyi-20@163.com>
2017-12-01 08:29:48 +0800

    优化首页爬虫代码

--- before/wubai.py
+++ before/wubai.py
@@ -10,18 +10,22 @@ con = req.read().decode('gbk')
 rs = re.findall(r'<tr zid=.*?</tr>', con, re.S)
 list1 = []
 for item in rs:
+    item = '<table>'+item+'</table>'
     soup = BeautifulSoup(item, 'html5lib')
-    league = soup.span.a.string
-    dict1 = {}
-    dict1['league'] = league
-    dict1['time'] = soup.span.span.attrs['title']
-    c = soup.span.select('a[title]')
-    dict1['host_team'] = c[0].string
-    dict1['away_team'] = c[1].string
-    list1.append(dict1)
+    tr = soup.find_all('tr')[0]
+    tds = tr.find_all('td')
+    list1.append({
+        'number': tds[0].a.contents[0],
+        'league': tds[1].text,
+        'time': tds[2].span.text,
+        'host_team': tds[3].a.contents[0],
+        'away_team': tds[5].a.contents[0],
+        'analyse': tds[8].a.attrs['href'],
+    })
 
 for i in list1:
-    print ('%s %s %s vs %s \n' %(i['time'], i['league'], i['host_team'], i['away_team'])) 
+    print ('%s %s %s %s vs %s \n %s \n' %(i['number'], i['time'], i['league'], i['host_team'], i['away_team'], i['analyse'])) 
     #print (i)
+
     
     
