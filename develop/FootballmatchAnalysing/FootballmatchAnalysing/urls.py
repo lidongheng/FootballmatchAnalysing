2836fdffda08adfc8750aa0f2acaaa4d82616496
lidongheng <huoyi-20@163.com>
2019-07-11 12:10:08 +0800

    restful init

--- develop/FootballmatchAnalysing/FootballmatchAnalysing/urls.py
+++ develop/FootballmatchAnalysing/FootballmatchAnalysing/urls.py
@@ -13,29 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url, include, path
 from django.contrib import admin
 from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers
 from match import views
+import quickstart.views
 
-# Serializers定义了API的表现.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets 定义了 视图（view） 的行为.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers 提供了一种简单途径，自动地配置了URL。
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
-# 使用自动的URL路由，让我们的API跑起来。
-# 此外，我们也包括了登入可视化API的URLs。
+router.register(r'users', quickstart.views.UserViewSet)
+router.register(r'groups', quickstart.views.GroupViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
